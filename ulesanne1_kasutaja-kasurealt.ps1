aaf9cec9f96d744556b333f8c49d293a54b649bd
Raini <raini.koorts@voco.ee>
2024-12-02 13:34:11 +0200

    Kasutaja loomine kontrolliga

---
+++ ulesanne1_kasutaja-kasurealt.ps1
@@ -0,0 +1,11 @@
+﻿$eesnimi = Read-Host "Sisesta oma eesnmimi"
+$perenimi = Read-Host "Sisesta oma perenimi"
+
+$parool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+$kasutajanimi = $("$eesnimi.ToLower().$perenimi.ToLower()")
+$kirjeldus = "$eesnimi $perenimi kasutaja"
+$taisnimi = $("$eesnimi $perenimi")
+if([bool](Get-LocalUser -ErrorAction SilentlyContinue $kasutajanimi)){
+    Write-Host  $kasutajanimi "Kasutaja on juba olemas" -ForegroundColor DarkRed}
+else{
+New-LocalUser -Name $taisnimi -Password $parool -FullName $taisnimi -Description $kirjeldus}
