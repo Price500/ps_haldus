a62b35c0c4b6546a7761847f52a6c14080f0a453
Raini <raini.koorts@voco.ee>
2024-12-02 13:12:56 +0200

    Parameetrid käsurealt

--- kasutaja_loomine_2.ps1
+++ kasutaja_loomine_2.ps1
@@ -1,8 +1,10 @@
-﻿param(
-    $username,
-    $fullname,
-    $description)
+﻿if($args.Count -ne 3){
+    Write-Host './skript kasutajanimi "Eesnimi Perenimi" "Konto kirjeldus"' -ForegroundColor DarkMagenta}
+else{
+    $username = $args[0]
+    $fullname = $args[1]
+    $description = $args[2]
 
 $salasõna = ConvertTo-SecureString "qwerty" -AsPlainText -Force
 
-New-LocalUser $username -Password $salasõna -FullName $fullname -Description $description
+New-LocalUser $username -Password $salasõna -FullName $fullname -Description $description}
