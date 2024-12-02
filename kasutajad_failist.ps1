d5f6cf828871f06b2381a06e834c98c9eb9d1c49
Raini <raini.koorts@voco.ee>
2024-12-02 12:48:00 +0200

    Kasutajate loomine faili abil

---
+++ kasutajad_failist.ps1
@@ -0,0 +1,10 @@
+﻿$fail = "C:\Users\Raini\ps_haldus\kasutajad.csv"
+$kasutajad = Import-Csv $fail -Encoding Default -Delimiter ";"
+
+foreach ($kasutaja in $kasutajad){
+    $kasutajanimi = $kasutaja.Kasutajanimi
+    $taisnimi = $kasutaja.Taisnimi
+    $kontokirjeldus = $kasutaja.KontoKirjeldus
+    $parool = $kasutaja.Parool | ConvertTo-SecureString -AsPlainText -Force
+
+    New-LocalUser -Name $kasutajanimi -Password $parool -FullName "$taisnimi" -Description "$kontokirjeldus"}
