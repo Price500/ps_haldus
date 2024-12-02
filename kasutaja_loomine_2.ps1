8d23e7e84bfaed1e3de2aafa1fc4c8c8196d503d
Raini <raini.koorts@voco.ee>
2024-12-02 12:33:14 +0200

    kasutaja loomine parameetritega

---
+++ kasutaja_loomine_2.ps1
@@ -0,0 +1,8 @@
+﻿param(
+    $username,
+    $fullname,
+    $description)
+
+$salasõna = ConvertTo-SecureString "qwerty" -AsPlainText -Force
+
+New-LocalUser $username -Password $salasõna -FullName $fullname -Description $description
