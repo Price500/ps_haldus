a319050f5938359d8b07734df9406e81f4295cf3
Raini <raini.koorts@voco.ee>
2024-12-02 13:35:21 +0200

    Ümbernimetus

--- kasutaja-kasurealt.ps1
+++
@@ -1,9 +0,0 @@
-﻿$eesnimi = Read-Host "Sisesta oma eesnmimi"
-$perenimi = Read-Host "Sisesta oma perenimi"
-
-$parool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
-$kasutajanimi = $("$eesnimi.ToLower().$perenimi.ToLower()")
-$kirjeldus = "$eesnimi $perenimi kasutaja"
-$taisnimi = $("$eesnimi $perenimi")
-
-New-LocalUser -Name $taisnimi -Password $parool -FullName $taisnimi -Description $kirjeldus
